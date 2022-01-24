f7ef95f3479e941e3f811613027099f7b0f7b407
Robert Xu <thecrxu@gmail.com>
2022-01-24 10:49:50 +0000

    sq

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
 
-before_action :authenticate_user!
+
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :is_guest, :login_guest
+  helper_method :is_guest, :login_guest, :current_user
 
   def current_user
-    super || guest_user
+    @current_user || guest_user
   end
 
   def is_guest?
@@ -23,7 +23,7 @@ before_action :authenticate_user!
   end
 
   def create_guest_user
-    user = User.create(:email => "anon_#{1+rand(99)}@ymous.com")
+    user = User.create(:email => "anon_#{1+rand(999999999)}@ymous.com")
     user.save(:validate => false)
     user
   end
