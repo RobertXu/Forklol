5048c02c52aaa9cea67e4c683092d6cb8abe1db7
Robert Xu <thecrxu@gmail.com>
2022-01-24 07:09:56 +0000

    restoring routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {:sessions => "user_sessions"}
+  devise_scope :user do
+      get 'user_sessions/login_guest' => 'user_sessions#login_guest'
+  end
+
+  root to: "static_pages#root"
+
+  namespace :api, defaults: { format: :json} do
+    resources :quizzes, only: [:index, :show, :update, :destroy, :create] do
+      resources :quiz_tables, only: [:index, :show]
+    end
+
+    resources :quiz_plays, only: [:update]
+
+    resources :quiz_tables, only: [:update, :destroy] do
+      resources :questions, only: [:index, :show, :update]
+    end
+  end
 end
+
