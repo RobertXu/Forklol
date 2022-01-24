fc74a75920c561cdec8b6220da867f7305a93622
Robert Xu <thecrxu@gmail.com>
2022-01-24 19:09:43 +0000

    sq

--- app/models/quiz_table.rb
+++ app/models/quiz_table.rb
@@ -1,5 +1,5 @@
 class QuizTable < ActiveRecord::Base
-  belongs_to :quiz, :class_name => "Quiz", :foreign_key => :quiz_id
+  belongs_to :quiz, optional: true, :class_name => "Quiz", :foreign_key => :quiz_id
   has_many :questions, :class_name => "Question", :foreign_key => :table_id
 
 end
